bbe076b41f582c277865c8602b321c0db8a66e98
zaraakhtar <zaramalikawan9@gmail.com>
2025-11-13 21:38:20 +0500

    added buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import ExploreBtn from "@/components/explorebtn"
 
 const page = () => {
   return (
-    <h1>welcome to nextjs 16</h1>
+    <section>
+      <h1 className="text-center">welcome to nextjs 16</h1>
+      <p className="text-center mt-5">Hechathon, Meetups and Confrences all in one</p>
+      <ExploreBtn />
+    </section>
   )
 }
 
---
+++ components/explorebtn.tsx
@@ -0,0 +1,8 @@
+'use client';
+const ExploreBtn = () => {
+  return (
+    <button className="ml-5" onClick={( )=> console.log("clicked")}>ExploreBtn</button>
+  )
+}
+
+export default ExploreBtn
